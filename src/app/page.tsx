b1e47f09fd4cfd6f9e5cb103a90345ee7424f638
walter potma <145157901+waltindobne@users.noreply.github.com>
2025-09-15 14:35:03 -0300

    batata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,18 @@ import Cube from "../components/cube/app";
 
 export default function Home() {
     return (
-        <div className="m-10 flex flex-direction flex-col items-center justify-center">
-            <div className="translate-y-6">
+        <div className="relativem-10 flex flex-direction flex-col items-center justify-center">
+            <div className="translate-y-6 z-20">
+                <Cube />
+            </div>
+            <div className="absolute top-25.5 z-10">
                 <Cube />
             </div>
             <div className="flex">
-                <div className="translate-x-14.5">
+                <div className="translate-x-14.5 z-20">
                     <Cube />
                 </div>
-                <div className="-translate-x-14.5">
+                <div className="-translate-x-14.5 z-20">
                     <Cube />
                 </div>
             </div>
